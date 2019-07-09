6d5c0d1d33d3736d888a3f8d2c627b093a097833
janders4 <johnanderson198689@gmail.com>
2019-07-09 17:42:34 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const key = require("./config");
+const app = express();
+const axios = require("axios");
+const cors = require("cors");
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/locations", getBreweries);
+
+function getBreweries(req, res, next) {
+  fetchBreweries()
+    .then(data => {
+      res.status(200).send(data);
+    })
+    .catch(err => console.log(err));
+}
+
+const fetchBreweries = () => {
+  return axios
+    .get(`https://sandbox-api.brewerydb.com/v2/locations/?key=${key.apiKey}`)
+    .then(({ data }) => {
+      return data;
+    });
+};
+
+module.exports = { app };
